079e6d0f20475d3df5a80e74547d5b4316826cec
Ascii175 <slimeyily@gmail.com>
2021-04-13 19:25:45 +0700

    up

--- mongoDBTest.py
+++ mongoDBTest.py
@@ -7,7 +7,7 @@ from flask import Flask,jsonify,render_template,request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb://admin:TEXmgk418600@node12706-monkuy.app.ruk-com.cloud:27017")  #ใส่username and passwd IP ของโหนด MongoDB
+client = pymongo.MongoClient("mongodb://admin:TEXmgk418600@10.100.2.33:27017")  #ใส่username and passwd IP ของโหนด MongoDB
 db = client["Ass1"]  #ชิ่อของDatabase
 
 
